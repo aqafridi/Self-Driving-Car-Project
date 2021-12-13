c312f8243f9d5d88e7946f3d3401c618bb5e2353
aqafridi <imaqafridi@gmail.com>
2021-12-14 00:59:09 +0500

    update

--- Behavoir Cloning/Main_test.py
+++ Behavoir Cloning/Main_test.py
@@ -1,3 +1,6 @@
+print('Setting UP')
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import socketio
 import eventlet
 import numpy as np
@@ -7,47 +10,52 @@ import base64
 from io import BytesIO
 from PIL import Image
 import cv2
-
+ 
+#### FOR REAL TIME COMMUNICATION BETWEEN CLIENT AND SERVER
 sio = socketio.Server()
-
-app = Flask(__name__) #'__main__'
-speed_limit = 10
-def img_preprocess(img):
+#### FLASK IS A MICRO WEB FRAMEWORK WRITTEN IN PYTHON
+app = Flask(__name__)  # '__main__'
+ 
+maxSpeed = 10
+ 
+ 
+def preProcess(img):
     img = img[60:135,:,:]
     img = cv2.cvtColor(img, cv2.COLOR_RGB2YUV)
     img = cv2.GaussianBlur(img,  (3, 3), 0)
     img = cv2.resize(img, (200, 66))
     img = img/255
     return img
-
-
+ 
+ 
 @sio.on('telemetry')
 def telemetry(sid, data):
     speed = float(data['speed'])
     image = Image.open(BytesIO(base64.b64decode(data['image'])))
     image = np.asarray(image)
-    image = img_preprocess(image)
+    image = preProcess(image)
     image = np.array([image])
-    steering_angle = float(model.predict(image))
-    throttle = 1.0 - speed/speed_limit
-    print('{} {} {}'.format(steering_angle, throttle, speed))
-    send_control(steering_angle, throttle)
-
-
-
+    steering = float(model.predict(image))
+    throttle = 1.0 - speed / maxSpeed
+    print(f'{steering}, {throttle}, {speed}')
+    sendControl(steering, throttle)
+ 
+ 
 @sio.on('connect')
 def connect(sid, environ):
     print('Connected')
-    send_control(0, 0)
-
-def send_control(steering_angle, throttle):
-    sio.emit('steer', data = {
-        'steering_angle': steering_angle.__str__(),
+    sendControl(0, 0)
+ 
+ 
+def sendControl(steering, throttle):
+    sio.emit('steer', data={
+        'steering_angle': steering.__str__(),
         'throttle': throttle.__str__()
     })
-
-
+ 
+ 
 if __name__ == '__main__':
-    model = load_model('my_model.h5')
+    model = load_model('model.h5')
     app = socketio.Middleware(sio, app)
+    ### LISTEN TO PORT 4567
     eventlet.wsgi.server(eventlet.listen(('', 4567)), app)
